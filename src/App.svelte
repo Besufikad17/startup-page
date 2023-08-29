18cfb48af8b2c0b04935dbade836abb34053cc77
besufikad17 <besufikadtilahun1994@gmail.com>
2023-08-29 14:58:26 +0300

    first commit, DateTime component added

---
+++ src/App.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import DateTime from "./lib/components/DateTime.svelte";
+</script>
+
+<main>
+  <div class="row">
+    <div class="col">
+      <DateTime/>
+    </div>
+  </div> 
+</main>
+
+<style>
+  .row {
+    display: flex;
+    flex-direction: column;
+  } 
+
+  .col {
+    display: flex;
+    flex-direction: row;
+    padding: 20px;
+  }
+</style>
---
+++ src/lib/components/DateTime.svelte
@@ -0,0 +1,56 @@
+<script lang="ts">
+  let now = new Date();
+  const monthNames = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+  ];
+
+  const dayNames = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday" 
+  ];
+
+</script>
+
+<main>
+ <div>
+   <p class="time">{now.getHours() + ":" + (now.getMinutes() < 10 ? "0" + now.getMinutes() : now.getMinutes())}</p>
+   <p class="day">{dayNames[now.getDay()] + ", " + monthNames[now.getMonth() + 1] + " " + now.getDate() + ", " + now.getFullYear()}</p>
+ </div>  
+</main>
+
+<style>
+  @font-face {
+    font-family: system-ui;
+  }
+
+  p {
+    margin: 0;
+  }
+
+  .time {
+    font-size: 32px;
+    font-family: "system-ui";
+  }
+
+  .day {
+    font-size: 20px;
+    font-family: "system-ui";
+  }
+
+</style>
